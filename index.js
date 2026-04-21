a29f4c110107a31fb833335aafaea79348605fff
LilyClausA <lilcla1230@students.maquoketaschools.org>
2026-04-21 18:59:00 +0000

    dcfg

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ function calculateSquare(){
     //add code here
     
 }
+
+//should multiply the input with the id number with itself 
+// and display the squared value in the div with the id result
+
+//should display 'Invalid, please enter a number' in the div 
+// with the id result when the input is not a number.
